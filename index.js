383708058cb92a32e0cacc2a3c847f9d40d64bcc
Vlad Zat <vlad.zat@mydit.ie>
2017-12-21 21:31:33 +0000

    add resolvers

--- index.js
+++ index.js
@@ -1,61 +1,39 @@
 const express = require('express');
-const app = express();
 
 const expressGraphQL = require('express-graphql');
 const { buildSchema } = require('graphql');
 
-const fs = require('fs');
-
+const app = express();
 const logger = require('./lib/logger');
 
-function generateSchema(typeName) {
-    let schema = 'type ' + typeName + ' {\n';
-
-    schema += '\thello: String\n';
-
-    schema += '}'
-
-    return buildSchema(schema);
-}
-
-function generateRoot() {
-    let root = {};
-
-    root['hello'] = function () {
-        return 'Hello World';
+const json =
+{
+    type: 'Query',
+    fields: {
+        test: {
+            params: {},
+            return: 'String'
+        },
+        test2: {
+            params: {
+                id: 'Int!',
+                name: 'String'
+            },
+            return: '[String]!'
+        }
     }
+};
 
-    return root;
-}
+const graphqlSchema = require('./lib/graphql/schema');
+const graphqlResolvers = require('./lib/graphql/resolvers');
+const schema = buildSchema(graphqlSchema.generate(json));
+const rootValue = graphqlResolvers.generate(json);
 
-const schema = generateSchema('Query');
-const rootValue = generateRoot();
-
-app.use('/graphql', expressGraphQL(async (req, res, params) => ({
+app.use('/graphql', expressGraphQL({
     schema: schema,
     rootValue: rootValue,
     graphiql: true
-})));
+}));
 
 app.listen(5000);
 logger.log('info', 'Data Retriever running on port 5000');
-
-const json = {
-                  type: "Query",
-                  fields: {
-                      test: {
-                          params: {},
-                          return: "String"
-                      },
-                      test2: {
-                          params: {
-                              id: "Int!",
-                              name: "String"
-                          },
-                          return: "[String]!"
-                      }
-                  }
-              }
-
-const graphqlSchema = require('./lib/graphql/schema');
-graphqlSchema.generate(json);
