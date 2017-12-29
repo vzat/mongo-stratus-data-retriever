86bfdc252c8953711def2a1232a174dd74655ef1
Vlad Zat <vlad.zat@mydit.ie>
2017-12-29 03:50:42 +0000

    Move to index

--- index.js
+++ index.js
@@ -1,60 +1,85 @@
-// const express = require('express');
-//
-// const expressGraphQL = require('express-graphql');
-// const { buildSchema } = require('graphql');
-//
-// const app = express();
-// const logger = require('./lib/logger');
-//
-// const json =
-// {
-//     type: 'Query',
-//     fields: {
-//         test: {
-//             params: {},
-//             return: 'String'
-//         },
-//         test2: {
-//             params: {
-//                 id: 'Int!',
-//                 name: 'String'
-//             },
-//             return: 'String'
-//         }
-//     }
-// };
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const expressGraphQL = require('express-graphql');
+const { buildSchema } = require('graphql');
+const removeRoute = require('express-remove-route');
+const request = require('request-promise');
 
-// {
-// 		"type": "Query",
-// 		"fields": {
-// 				"test": {
-// 						"params": {},
-// 						"return": "String"
-// 				},
-// 				"test2": {
-// 						"params": {
-// 								"id": "Int!"
-// 						},
-// 						"return": "String"
-// 				}
-// 		}
-// }
+const logger = require('./lib/logger');
+const routes = require('./api/v1/routes');
+const graphqlSchema = require('./lib/graphql/schema');
+const graphqlResolvers = require('./lib//graphql/resolvers');
 
+const app = express();
 
+function getToken(data) {
+    return 'abc123';
+}
 
-//
-// const graphqlSchema = require('./lib/graphql/schema');
-// const graphqlResolvers = require('./lib/graphql/resolvers');
-// const schema = buildSchema(graphqlSchema.generate(json));
-// const rootValue = graphqlResolvers.generate(json);
-//
-// app.use('/graphql', expressGraphQL({
-//     schema: schema,
-//     rootValue: rootValue,
-//     graphiql: true
-// }));
-//
-// app.listen(5000);
-// logger.log('info', 'Data Retriever running on port 5000');
+function getUser(token) {
+    return 'john';
+}
 
-const app = require('./lib/express-api.js');
+app.set('port', process.env.PORT || 5000);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+// Debug only
+app.get('/', function (req, res) {
+    res.end('Data Retriever');
+});
+
+routes.put('/:database/schema', function (req, res) {
+    const database = req.params.database;
+    const token = getToken(req.get('Authorization'));
+    const data = req.body.schema;
+
+    const user = getUser(token);
+
+    const schema = buildSchema(graphqlSchema.generate(data));
+    const rootValue = graphqlResolvers.generate(data);
+
+    removeRoute(app, '/api/v1/' + user + '/' + database);
+
+    const options = {
+        method: 'GET',
+        uri: 'http://localhost:' + app.get('port') + '/api/v1/' + user + '/' + database,
+    };
+
+    new Promise(function (resolve, reject) {
+        request(options)
+            .then(function (body) {
+                logger.log('info', body.statusCode);
+            })
+            .catch(function (err) {
+                logger.log('info', err.statusCode);
+
+                if (err.statusCode == 404) {
+                    resolve();
+                }
+            });
+    });
+
+    app.use('/api/v1/' + user + '/' + database, expressGraphQL({
+        schema: schema,
+        rootValue: rootValue,
+
+        // Debug only
+        graphiql: true
+    }));
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.send(JSON.stringify({'error': 0}));
+});
+
+app.listen(app.get('port'), function () {
+    logger.log('info', 'Data Retriever running on port ' + app.get('port'));
+
+    app.use('/api/v1', routes);
+});
+
+module.exports = app;
