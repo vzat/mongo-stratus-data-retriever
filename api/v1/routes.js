9452cbd2661a84023a365b77d5d3a6af9f8783bd
Vlad Zat <vlad.zat@mydit.ie>
2018-02-04 16:40:45 +0000

    Add schema to db

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -1,15 +1,65 @@
 const express = require('express');
 
 const logger = require('../../lib/logger');
+const graphqlGenerator = require('../../lib/graphql-generator');
+const removeEndpoint = require('../../lib/express-remove-endpoint');
+const utils = require('../../lib/utils');
+const db = require('../../lib/db');
+const authMiddleware = require('../../lib/authMiddleware');
 
 const router = express.Router();
 
-function getToken(data) {
-    return 'abc123';
+async function addSchemaToSysDB (username, dbName, schemaData) {
+    try {
+        const docs = await db.getDocumentsSysDB('accounts', {'username': username});
+
+        for (const dNo in docs[0].databases) {
+            if (docs[0].databases[dNo].name === dbName) {
+                docs[0].databases[dNo].schema = JSON.stringify(schemaData);
+            }
+        }
+
+        await db.updateDocumentSysDB('accounts', {'username': username}, docs[0]);
+    }
+    catch (err) {
+        logger.log('error', err);
+    }
 }
 
-function getUser(token) {
-    return 'john';
-}
+const routes = function (app) {
+    router.put('/:user/:database/schema', authMiddleware, async function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
 
-module.exports = router;
+        try {
+            const database = req.params.database;
+            const user = req.params.user;
+            const token = utils.getToken(req.get('Authorization'));
+            const schemaData = req.body.schema;
+
+            // Add Schema to the Database
+            addSchemaToSysDB(user, database, schemaData);
+
+            removeEndpoint(app, '/api/v1/' + user + '/' + database);
+
+            const userData = {
+                username: user,
+                database: database,
+                token: token
+            };
+
+            graphqlGenerator.createEndpoint(app, '/api/v1/', userData, schemaData);
+
+            res.send(JSON.stringify({'error': 0}));
+        }
+        catch (e) {
+            logger.log('error', e);
+            res.send(JSON.stringify({'error': 1, 'errorMessage': e}));
+        }
+    });
+
+    // TODO: Add endpoint to change the API token
+
+    return router;
+};
+
+module.exports = routes;
--- index.js
+++ index.js
@@ -3,25 +3,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const logger = require('./lib/logger');
-const routes = require('./api/v1/routes');
-const graphqlGenerator = require('./lib/graphql-generator');
-const removeEndpoint = require('./lib/express-remove-endpoint');
-const utils = require('./lib/utils');
-const db = require('./lib/db');
-const authMiddleware = require('./lib/authMiddleware');
-
 let app = express();
 
-// async function getUser (token) {
-//     const docs = await db.getDocumentsSysDB('accounts', {'token': token}, {'projection': {'username': 1}});
-//
-//     if (docs !== undefined && docs.length === 1) {
-//         return docs[0].username;
-//     }
-//
-//     return undefined;
-// }
+const logger = require('./lib/logger');
+const routes = require('./api/v1/routes')(app);
+const db = require('./lib/db');
 
 db.connectSysDB();
 
@@ -35,87 +21,10 @@ app.get('/', function (req, res) {
     res.end('Data Retriever');
 });
 
-routes.put('/:user/:database/schema', authMiddleware, async function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-
-    try {
-        const database = req.params.database;
-        const user = req.params.user;
-        const token = utils.getToken(req.get('Authorization'));
-        const schemaData = req.body.schema;
-
-        // const user = await getUser(token);
-
-        removeEndpoint(app, '/api/v1/' + user + '/' + database);
-
-        const userData = {
-            username: user,
-            database: database,
-            token: token
-        };
-
-        graphqlGenerator.createEndpoint(app, '/api/v1/', userData, schemaData);
-
-        res.send(JSON.stringify({'error': 0}));
-    }
-    catch (e) {
-        logger.log('error', e);
-        res.send(JSON.stringify({'error': 1, 'errorMessage': e}));
-    }
-});
-
-// TODO: Add endpoint to change the API token
-
 app.use('/api/v1', routes);
 
 app.listen(app.get('port'), function () {
     logger.log('info', 'Data Retriever running on port ' + app.get('port'));
 });
-
-
-
-// Testing new resolver generating functions
-// const tempSchema = {
-//     "Query": {
-//         "collection1": "[Collection1.Document]",
-//         "collection2": "[Collection2.Document]"
-//     },
-//     "Collection1.Document": {
-//         "_id": "ID",
-//         "field1": "String",
-//         "field2": "Int",
-//         "field3": "Test"
-//     },
-//     "Test": {
-//         "test1": "String",
-//         "test2": "String"
-//     },
-//     "Collection2.Document": {
-//         "_id": "ID",
-//         "field9": "Int",
-//         "field8": "Boolean"
-//     }
-// };
-
-// const resolversv2 = require('./lib/graphql-generator/resolversv2');
-// resolversv2.generate(tempSchema);
-
-
-// const db = require('./lib/db.js');
-
-// db.connectSysDB().then(() => {
-//     logger.log('info', 'After?');
-//     // db.getDocument('mongodb://localhost:27017/mongoStratus', 'abc', '5a6ce262dbb5d8ce801f98f3', ['abc']);
-//     // db.getDocuments('mongodb://localhost:27017/mongoStratus', 'abc', ['abc']);
-//     const serverInfo = {
-//         ip: 'localhost',
-//         port: 27017,
-//         name: 'mongoStratus'
-//     }
-//     db.getDocumentsv2(serverInfo, 'abc', {'abc': 'def'}, {'projection': {'abc': 1}});
-// });
-
-// const utils = require('./lib/utils');
-// console.log(utils.getDBFromUrl('/api/v1/john/abc'));
 
 module.exports = app;
