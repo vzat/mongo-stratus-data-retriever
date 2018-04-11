3b9728d08b6127559d173cf713621145f0462872
Vlad Zat <vlad.zat@mydit.ie>
2018-04-11 03:14:18 +0100

    Add Integration Test

---
+++ test/integration/api.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const expect = require('chai').expect;
+
+const app = require('../../index');
+
+describe('API', () => {
+    describe('REST', () => {
+        describe('GET /api/v1/<user>/<instance>/databases', () => {
+            it('should get all the databases', (done) => {
+                request(app)
+                    .get('/api/v1/admin/mongoStratus/databases')
+                    .set('Authorization', 'Bearer z321')
+                    .end((err, res) => {
+                        expect(err).to.equal(null);
+                        expect(res.statusCode).to.equal(200);
+                        expect(res).to.have.property('body');
+                        expect(res.body).to.have.property('ok');
+                        expect(res.body).to.have.property('data');
+                        done();
+                    });
+            });
+        });
+    });
+});
